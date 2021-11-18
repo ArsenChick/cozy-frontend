a3bf729c7e79fa91b036e261ce154c6f56879dda
Arseniy <arseniy1304@gmail.com>
2021-11-18 17:34:52 +0300

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { WatchComponent } from './watch/watch.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    TopBarComponent,
+    WatchComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/videos.ts
@@ -0,0 +1,27 @@
+export interface Video {
+  id: string;
+  title: string;
+  duration: number;
+  thumbnail: string;
+}
+
+export const videos = [
+  {
+    id: 'w4897taac_2',
+    title: 'Cute puppies <3',
+    duration: 122,
+    thumbnail: 'Thumbnail pic'
+  },
+  {
+    id: 'htSA22k9r1S',
+    title: 'OMG! I CAN\'T BELIEVE',
+    duration: 921,
+    thumbnail: 'Thumbnail pic'
+  },
+  {
+    id: '1roiubr1111',
+    title: 'daily vlog #2',
+    duration: 9003,
+    thumbnail: 'Thumbnail pic'
+  },
+];
